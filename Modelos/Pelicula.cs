5bba5cc933b5ca133295f0cf30ff8e7be4fb3b14
QiangRO <aroch22222@hotmail.com>
2022-08-26 12:43:03 -0400

    Agregar archivos de proyecto.

---
+++ Modelos/Pelicula.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TiendaPelicula.Modelos
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Fecha Publicacion")]
+        [DataType(DataType.Date)]
+        public DateTime FechaPublicacion { get; set; }
+        public string Genero { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Precio { get; set; }
+    }
+}
---
+++ Pages/Peliculas/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TiendaPelicula.Data;
+using TiendaPelicula.Modelos;
+
+namespace TiendaPelicula.Pages.Peliculas
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TiendaPelicula.Data.TiendaPeliculaContext _context;
+
+        public IndexModel(TiendaPelicula.Data.TiendaPeliculaContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Pelicula> Pelicula { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Pelicula != null)
+            {
+                Pelicula = await _context.Pelicula.ToListAsync();
+            }
+        }
+    }
+}
